282be28a7b31c41927a5a726740a13a546364266
Jeremy J. Lee <jeremyjlee@dhcp-10-142-227-25.uawifi.arizona.edu>
2018-03-06 21:58:11 -0700

    working app, form doesn't clear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,8 @@ var empName = "";
 var role = "";
 var startDate = "";
 var monthlyRate = "";
+var randomFormat = "DD/MM/YY";
+// var convertedDate = moment(startDate, randomFormat);
 
 //capture button on click
 $("#add-employee-btn").on("click", function () {
@@ -23,7 +25,7 @@ $("#add-employee-btn").on("click", function () {
 
     empName = $("#employee-name-input").val().trim();
     role = $("#role-input").val().trim();
-    startDate = $("#start-input").val().trim();
+    startDate = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
     monthlyRate = $("#rate-input").val().trim();
 
     database.ref().set({
@@ -32,6 +34,7 @@ $("#add-employee-btn").on("click", function () {
         startDate: startDate,
         monthlyRate: monthlyRate
     });
+    $(".form-control").empty();
 })
 
 //firebase watcher + initial loader
@@ -39,16 +42,20 @@ $("#add-employee-btn").on("click", function () {
 database.ref().on("value", function (data) {
     console.log(data.val());
     // Get reference to existing tbody element, create a new table row element
-    var tBody = $("tbody");
+    var tBody = $("tBody");
     var tRow = $("<tr>");
+    var child = data.val();
     // Methods run on jQuery selectors return the selector they we run on
     // This is why we can create and save a reference to a td in the same statement we update its text
-    var empNameTd = $("<td>").text(data.empName);
-    var roleTd = $("<td>").text(data.role);
-    var startDateTd = $("<td>").text(data.startDate);
-    var monthlyRateTd = $("<td>").text(data.monthlyRate);
+    var empNameTd = $("<td>").text(child.empName);
+    var roleTd = $("<td>").text(child.role);
+    var startDateTd = $("<td>").text(moment.unix(child.startDate).format("MM/DD/YY"));
+    var monthlyRateTd = $("<td>").text(child.monthlyRate);
+    // var empStartPretty = 
+    var empMonthsTd = $("<td>").text(moment().diff(moment.unix(child.startDate, "X"), "months"));
+    console.log(empMonthsTd);
     // Append the newly created table data to the table row
-    tRow.append(empNameTd, roleTd, startDateTd, monthlyRateTd);
+    tRow.append(empNameTd, roleTd, startDateTd, empMonthsTd, monthlyRateTd);
     // Append the table row to the table body
     tBody.append(tRow);
 
